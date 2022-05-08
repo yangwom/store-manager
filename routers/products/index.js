dd6b17118b363bd8b8b80618654e1ee5f7f9065d
yang vieira <yangflix3@gmail.com>
2022-05-08 16:00:43 -0400

    fix rounter index

--- routers/products/index.js
+++ routers/products/index.js
@@ -4,5 +4,6 @@ const controllers = require('../../controllers/productsControllers');
 const routerProducts = express.Router();
 
 routerProducts.get('/', controllers.getAll);
+routerProducts.get('/:id');
 
 module.exports = routerProducts;
