c503ac56c182b1c4c3149059b005b26b7a1dcf4b
yang vieira <yangflix3@gmail.com>
2022-05-08 19:29:31 -0400

    fix: status

--- status/status.js
+++ status/status.js
@@ -2,7 +2,7 @@ const success = 200;
 const notFound = 404;
 const badRequest = 400;
 const internalServerError = 500;
-const productNotFound = { status: 404, message: 'Product not found' };
+const productNotFound = { status: notFound, message: 'Product not found' };
 
 module.exports = {
 success,
