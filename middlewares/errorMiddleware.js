996cb2dc6426677301307c063c183f654515e8d7
yang vieira <yangflix3@gmail.com>
2022-05-08 15:28:49 -0400

    errorMiddleware

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,1 +1,6 @@
-// Renomeie esse arquivo
+const errorMiddlewares = (err, req, res, _next) => {
+if (err.status) return res.status(err.status).json({ message: err.message });
+return res.status(500).json({ message: 'erro interno' });
+};
+
+module.exports = errorMiddlewares;
