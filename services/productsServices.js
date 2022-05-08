0d167bbd63ea6e82d23f539ccc7011b49b2c3553
yang vieira <yangflix3@gmail.com>
2022-05-08 19:22:03 -0400

    product services

--- services/productsServices.js
+++ services/productsServices.js
@@ -1,5 +1,5 @@
 const getAllModel = require('../models/productsModel');
-const error = require('../status/status');
+const status = require('../status/status');
 
 const getAll = async () => {
 const data = await getAllModel.getAll();
@@ -11,7 +11,9 @@ if (!data.length) return [];
 
 const getById = async (id) => {
 const data = await getAllModel.getById(id);
-if (!data) throw error.productNotFound;
+
+if (!data) throw status.productNotFound;
+
 return data;
 };
 
