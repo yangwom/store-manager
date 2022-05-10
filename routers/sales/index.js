aa8bd49fee7023a47abeeacebbe380cc7e48e0da
yang vieira <yangflix3@gmail.com>
2022-05-10 16:33:25 -0400

    getById sales

--- routers/sales/index.js
+++ routers/sales/index.js
@@ -4,5 +4,6 @@ const controllers = require('../../controllers/salesControllers');
 const routerSales = express.Router();
 
 routerSales.get('/', controllers.getAll);
+routerSales.get('/:id', controllers.getById);
 
 module.exports = routerSales;
