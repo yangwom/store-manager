d3fb27c1bafd8c5ccfad8762212cf74ad5f70bfb
yang vieira <yangflix3@gmail.com>
2022-05-10 15:03:04 -0400

    controllers get all

--- controllers/salesControllers.js
+++ controllers/salesControllers.js
@@ -2,9 +2,9 @@ const servicesGetAll = require('../services/salesServices');
 const status = require('../status/status');
 
 const getAll = async (req, res, next) => {
-const data = await servicesGetAll.getAll();
 try {
-  res.status(status.success).json(data);
+ const data = await servicesGetAll.getAll();
+ return res.status(status.success).json(data);
 } catch (err) {
     next(err);
 }
