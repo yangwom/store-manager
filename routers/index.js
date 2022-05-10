abe1e389583d7ab1bcbf2e7880ade0421a995e07
yang vieira <yangflix3@gmail.com>
2022-05-10 12:58:26 -0400

    fix routers

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const routerProducts = require('./products/index');
+const routerSales = require('./sales/index');
 
 const router = express.Router();
 
 router.use('/products', routerProducts);
+router.use('/sales', routerSales);
 
 module.exports = router;
