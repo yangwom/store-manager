4705dfcd681ee860c27e758e4eb49576bc734f80
yang vieira <yangflix3@gmail.com>
2022-05-11 11:07:58 -0400

    fix validate

--- middlewares/validateMiddleware.js
+++ middlewares/validateMiddleware.js
@@ -7,6 +7,7 @@ const canNotLower1 = '"quantity" must be greater than or equal to 1';
 const productIdIsRequired = '"productId" is required';
 
 const productMiddleware = (req, res, next) => {
+    // testando com switch
     const { name, quantity } = req.body; 
     switch (true) {
     case !name: return next({ status: status.badRequest, message: nameIsRequired });
@@ -19,14 +20,13 @@ const productMiddleware = (req, res, next) => {
     };
 
     const salesMiddleware = (req, res, next) => {
-      const { productId, quantity } = req.body;
-      switch (true) {
-          case !quantity: return next({ status: status.badRequest, message: quantityIsRequired });
-          case !productId: return next({ status: status.badRequest, message: productIdIsRequired });
-          case quantity <= 0: return next({ status: status.Unprocessa, message: canNotLower1 });
-       default:
-           next();
-      }
+      const data = req.body;
+      data.forEach(({ productId, quantity }) => {
+        if (!quantity) return next({ status: status.badRequest, message: quantityIsRequired });
+        if (!productId) return next({ status: status.badRequest, message: productIdIsRequired });
+        if (quantity <= 0) return next({ status: status.Unprocessa, message: canNotLower1 });
+       });
+       next();
     };
 
 module.exports = {
