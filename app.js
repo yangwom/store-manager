8315f27bf5ea7f932dc6d13716cf95cf5cacbcf5
yang vieira <yangflix3@gmail.com>
2022-05-11 20:47:23 -0400

    fix app.js

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const router = require('./routers/index');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const app = express();
+app.use(express.json());
+app.use(router);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
-app.use(express.json());
-app.use(router);
 app.use(errorMiddleware);
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
