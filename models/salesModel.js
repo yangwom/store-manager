22228172cab315898a6db527d936da8c3ef9ee08
yang vieira <yangflix3@gmail.com>
2022-05-12 11:18:35 -0400

    feat(function createsales e createsalesproduct): insert into tables requisito 07

--- models/salesModel.js
+++ models/salesModel.js
@@ -23,7 +23,21 @@ const getById = async (id) => {
     return data;
 };
 
+const createSaleProduct = async (productId, quantity) => {
+const query = 'INSERT INTO sales_product (product_id,  quantity) values (?,?)';
+const [data] = connection.execute(query, [productId, quantity]); 
+return data;
+};
+
+const createSales = async (date) => {
+const query = 'INSERT INTO sales (date) values (NOW())';
+const [data] = connection.execute(query, [date]);
+return data;
+};
+
 module.exports = {
     getAll,
     getById,
+    createSaleProduct,
+    createSales,
 };
